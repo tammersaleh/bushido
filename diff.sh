fef2d589cf3b5b489f64eda3a32f46077942fc53
Tammer Saleh <tsaleh@gmail.com>
2009-10-17 09:40:03 -0700

    changed location of temp-app so as not to screw up git

--- diff.sh
+++ diff.sh
@@ -4,9 +4,9 @@ set -e
 
 if [ $# == 1 ]; then
   echo "gold vs test-app"
-  diff -wB {gold,test-app}/$1 | less
+  diff -wB gold/$1 /tmp/test-app/$1 | less
 else
-  diff -wBrq -x '*.swp' gold test-app | less
+  diff -wBrq -x '*.swp' gold /tmp/test-app | less
 fi
 
 
--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 set -e
 
 clear 
+cd /tmp
 rm -rf test-app 
 rails test-app -m /Users/tsaleh/code/tsaleh-instant-rails-app/template.rb
 ./diff.sh
