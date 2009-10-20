8fbc355a1e6eb49db4806975cd455ef1605fff48
Tammer Saleh <tsaleh@gmail.com>
2009-10-20 12:35:00 -0700

    Small fixes to scripts

--- diff.sh
+++ diff.sh
@@ -6,7 +6,11 @@ if [ $# == 1 ]; then
   echo "gold vs test-app"
   diff -wB gold/$1 /tmp/test-app/$1 | less
 else
-  diff -wBrq -x '*.swp' gold /tmp/test-app | less
+  diff -wBrq -x '*.swp' \
+             -x '.git' \
+             -x 'hoptoad.rb' \
+             -x 'session_store.rb' \
+             gold /tmp/test-app | less
 fi
 
 
--- run.sh
+++ run.sh
@@ -4,6 +4,9 @@ set -e
 
 clear 
 
+rm gold/log/*.log
+rm gold/db/schema.rb
+
 cd /tmp
   rm -rf test-app 
   rails test-app -m /Users/tsaleh/code/tsaleh-instant-rails-app/template.rb
